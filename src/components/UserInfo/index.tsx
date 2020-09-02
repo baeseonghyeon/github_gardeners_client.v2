8157170ccec014a0b5e0146fdc047d6482c23213
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-09-02 12:38:03 +0900

    hotfix #6
    - 메인페이지 overlfow 해결
    - 유저 링크 변경
    - 기타 UI 오류사항 해결

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -13,16 +13,16 @@ interface UserInfoProps {
     title_size?: string,
     desc_size?: string,
     isVertical?: boolean,
-    Badge? : ReactNode,
-    onClick?: (event:React.MouseEvent)=>void,
+    Badge?: ReactNode,
+    onClick?: (event: React.MouseEvent) => void,
 }
 
 const UserInfo = (props: UserInfoProps) => {
     const _avatar_url = isNullOrUndefined(props.avatar_url) ? Github.getAvatarUrl(props.id) : props.avatar_url
 
-    return <div 
+    return <div
         className={`__user_info_container ${props.className} ${!isNullOrUndefined(props.isVertical) && props.isVertical ? "vertical" : ""}`}
-        onClick={ props.onClick }
+        onClick={props.onClick}
     >
         <div className="__user_info_avatar">
             <img
@@ -35,22 +35,22 @@ const UserInfo = (props: UserInfoProps) => {
                 }
             ></img>
             <div className="__user_info_avatar_verified">
-                { props.Badge }
+                {props.Badge}
             </div>
 
         </div>
         <div className="__user_info_desc">
-            <a 
-                style={ !isNullOrUndefined(props.title_size) ? { fontSize: props.title_size } : {} }
-                className="__user_info_login" 
-                href={`https://github.com/${props.login}`} target="_blank" rel="noopener noreferrer">
+            <a
+                style={!isNullOrUndefined(props.title_size) ? { fontSize: props.title_size } : {}}
+                className="__user_info_login"
+                href={`/users/${props.login}`} target="_blank" rel="noopener noreferrer">
                 {props.login}
             </a>
             {
                 (!isNullOrUndefined(props.name) || !isNullOrUndefined(props.user_name)) ?
-                    <p 
-                    style={ !isNullOrUndefined(props.desc_size) ? { fontSize: props.desc_size } : {} }
-                    className="__user_info_username">{(props.name || props.user_name)}</p> :
+                    <p
+                        style={!isNullOrUndefined(props.desc_size) ? { fontSize: props.desc_size } : {}}
+                        className="__user_info_username">{(props.name || props.user_name)}</p> :
                     <></>
             }
         </div>
--- src/views/MainView/UserRankList.tsx
+++ src/views/MainView/UserRankList.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React , { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './scss/UserRankList.scss';
 
@@ -9,48 +9,48 @@ import { getUsersRankList, IUsersRankListItem } from '../../api/analytics';
 import IChallenge from '../../api/interfaces/Challenge';
 import { isNullOrUndefined } from 'util';
 
-import {FaMedal} from 'react-icons/fa' ;
+import { FaMedal } from 'react-icons/fa';
 
 interface UserRankListProps {
-    selectedChallenge : IChallenge | null,
+    selectedChallenge: IChallenge | null,
 }
 
-const UserRankList = (props:UserRankListProps)=>{
-    const [ranks , setRanks] = useState<IUsersRankListItem[]>([]); 
+const UserRankList = (props: UserRankListProps) => {
+    const [ranks, setRanks] = useState<IUsersRankListItem[]>([]);
     const fn = {
-        fetch : async ()=>{
-            if(!isNullOrUndefined(props.selectedChallenge)){
+        fetch: async () => {
+            if (!isNullOrUndefined(props.selectedChallenge)) {
                 const result = await getUsersRankList(props.selectedChallenge.id);
-                if(result.code > 0){
+                if (result.code > 0) {
                     setRanks(result.data);
                 }
             }
         }
     }
     useEffect(() => {
-        if(!isNullOrUndefined(props.selectedChallenge)){
+        if (!isNullOrUndefined(props.selectedChallenge)) {
             fn.fetch();
         }
     }, [props, props.selectedChallenge])
-    return <Card 
-    header={{
-        title : "출석 순위",
-        desc : "정원사분들의 출석 순위입니다. 10등까지 노출됩니다."
-    }}
-    className="user-rank-list-container" 
-    wrapperClassName="user-rank-list-wrapper">
+    return <Card
+        header={{
+            title: "출석 순위",
+            desc: "정원사분들의 출석 순위입니다. 10등까지 노출됩니다."
+        }}
+        className="user-rank-list-container"
+        wrapperClassName="user-rank-list-wrapper">
         {
-            ranks.map((item,idx)=>{
-                return <div className="user-rank-item" key={ idx } >
-                    <p className={`user-rank-text ${ item.rank < 4 ? "rank-" + item.rank : "" }`}>
-                        <b>{ item.rank }</b>
-                        등
-                    </p>
-                    <UserInfo 
-                    className={ `user-rank-item-info ${ item.rank < 4 ? "rank-" + item.rank : "" }` }
-                    Badge={ item.rank < 4 ? <FaMedal/> : <></> }
-                    { ...item.info }
-                />
+            ranks.map((item, idx) => {
+                return <div className="user-rank-item" key={idx} >
+                    <p className={`user-rank-text ${item.rank < 4 ? "rank-" + item.rank : ""}`}>
+                        <b>{item.rank}</b>
+                            등
+                        </p>
+                    <UserInfo
+                        className={`user-rank-item-info ${item.rank < 4 ? "rank-" + item.rank : ""}`}
+                        Badge={item.rank < 4 ? <FaMedal /> : <></>}
+                        {...item.info}
+                    />
                 </div>
             })
         }
